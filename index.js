d86105b851a1a916a535eef0e3df5cfcb9418132
Alireza Jafarlou <mr.alj.work@gmail.com>
2026-08-12 12:45:14 +0330

    feat: add service creation endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+const services = [];
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
@@ -16,6 +18,18 @@ app.get("/about", (req, res) => {
         name: "health-api",
         description: "a health checker api"
     });
+});
+
+app.post("/services", (req, res) => {
+    const service = {
+        id: services.length + 1,
+        name: req.body.name,
+        url: req.body.url
+    };
+
+    services.push(service);
+
+    res.status(201).json(service);
 });
 
 if (require.main === module) {
--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("./index");
+const expectCookies = require("supertest/lib/cookies");
 
 describe("health-api", () => {
     test("GET / returns the application message", async () => {
@@ -21,5 +22,17 @@ describe("health-api", () => {
 
         expect(response.statusCode).toBe(200);
         expect(response.body.description).toBe("a health checker api");
+    });
+
+    test("POST /services sends info about the given service", async () => {
+        const response = await request(app)
+        .post("/services")
+        .send({
+            name: "my-api",
+            url: "https://example.com"
+        });
+
+        expect(response.statusCode).toBe(201);
+        expect(response.body.name).toBe("my-api")
     });
 });
